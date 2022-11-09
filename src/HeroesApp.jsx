59e1e09e2e75cb717dcbde38a8a1e13a9c3c60dc
Santiago Cifuentes <cifuentess938@gmail.com>
2022-11-08 21:17:57 -0500

    adding the context

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,12 +1,13 @@
+import { AuthProvider } from "./auth"
 import { AppRouter } from "./router/AppRouter"
 
 
 export const HeroesApp = () => {
     return (
-        <>
+        <AuthProvider>
             <AppRouter/>
             {/* <h1>Heroes app</h1> */}
-        </>
+        </AuthProvider>
     )
 }
 
